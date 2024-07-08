42e5e9550fb4d8caff8e1cc24b3c761d76711281
Ruzgfpegk <Ruzgfpegk@zero-sum.fr>
2024-07-08 19:34:02 +0200

    Add support for Browser sessions.

---
+++ src/Connections/Browser.php
@@ -0,0 +1,11 @@
+<?php
+declare( strict_types=1 );
+
+namespace Ruzgfpegk\Sessionator\Connections;
+
+/**
+ * The Browser object extends the Common object with specificities of Browser connections
+ */
+class Browser extends Common {
+	// Nothing special for this format.
+}
